b17020003cc91f6d99f147b6188a4ee2b4991666
YANYAN <YANYAN@YANYAN>
2024-10-07 00:48:09 +0800

    Add project files.

---
+++ Info/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace Info
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox_ID = new System.Windows.Forms.TextBox();
+            this.textBox_FIRSTNAME = new System.Windows.Forms.TextBox();
+            this.textBox_LASTNAME = new System.Windows.Forms.TextBox();
+            this.textBox_Position = new System.Windows.Forms.TextBox();
+            this.button_Submit = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.Font = new System.Drawing.Font("Cooper Black", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(10, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Add Employee:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label2.Font = new System.Drawing.Font("Cooper Black", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(297, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(185, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Employee list:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Gainsboro;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ID no.*";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Gainsboro;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(11, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "First name*";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Gainsboro;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 196);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Last name*";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Gainsboro;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(11, 252);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Position*";
+            // 
+            // textBox_ID
+            // 
+            this.textBox_ID.BackColor = System.Drawing.Color.White;
+            this.textBox_ID.Location = new System.Drawing.Point(28, 104);
+            this.textBox_ID.Name = "textBox_ID";
+            this.textBox_ID.Size = new System.Drawing.Size(205, 20);
+            this.textBox_ID.TabIndex = 6;
+            this.textBox_ID.TextChanged += new System.EventHandler(this.textBox_ID_TextChanged);
+            // 
+            // textBox_FIRSTNAME
+            // 
+            this.textBox_FIRSTNAME.BackColor = System.Drawing.Color.White;
+            this.textBox_FIRSTNAME.Location = new System.Drawing.Point(29, 156);
+            this.textBox_FIRSTNAME.Name = "textBox_FIRSTNAME";
+            this.textBox_FIRSTNAME.Size = new System.Drawing.Size(205, 20);
+            this.textBox_FIRSTNAME.TabIndex = 7;
+            // 
+            // textBox_LASTNAME
+            // 
+            this.textBox_LASTNAME.BackColor = System.Drawing.Color.White;
+            this.textBox_LASTNAME.Location = new System.Drawing.Point(28, 229);
+            this.textBox_LASTNAME.Name = "textBox_LASTNAME";
+            this.textBox_LASTNAME.Size = new System.Drawing.Size(205, 20);
+            this.textBox_LASTNAME.TabIndex = 8;
+            // 
+            // textBox_Position
+            // 
+            this.textBox_Position.BackColor = System.Drawing.Color.White;
+            this.textBox_Position.Location = new System.Drawing.Point(29, 288);
+            this.textBox_Position.Name = "textBox_Position";
+            this.textBox_Position.Size = new System.Drawing.Size(205, 20);
+            this.textBox_Position.TabIndex = 9;
+            // 
+            // button_Submit
+            // 
+            this.button_Submit.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button_Submit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Submit.Location = new System.Drawing.Point(39, 326);
+            this.button_Submit.Name = "button_Submit";
+            this.button_Submit.Size = new System.Drawing.Size(133, 36);
+            this.button_Submit.TabIndex = 10;
+            this.button_Submit.Text = "Submit";
+            this.button_Submit.UseVisualStyleBackColor = false;
+            this.button_Submit.Click += new System.EventHandler(this.button_Submit_Click);
+            // 
+            // dataGridView1
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Transparent;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle1.NullValue = null;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.Disable;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Transparent;
+            dataGridViewCellStyle2.NullValue = null;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridView1.Location = new System.Drawing.Point(262, 39);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.HotTrack;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.dataGridView1.RowTemplate.DefaultCellStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(439, 269);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.VirtualMode = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(708, 365);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button_Submit);
+            this.Controls.Add(this.textBox_Position);
+            this.Controls.Add(this.textBox_LASTNAME);
+            this.Controls.Add(this.textBox_FIRSTNAME);
+            this.Controls.Add(this.textBox_ID);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Employee Details";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox_ID;
+        private System.Windows.Forms.TextBox textBox_FIRSTNAME;
+        private System.Windows.Forms.TextBox textBox_LASTNAME;
+        private System.Windows.Forms.TextBox textBox_Position;
+        private System.Windows.Forms.Button button_Submit;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Info/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Drawing;  
+
+namespace Info
+{
+    public partial class Form1 : Form
+    {
+        private BindingList<Employee> employees;  
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            employees = new BindingList<Employee>();
+            dataGridView1.DataSource = employees;
+
+           
+            dataGridView1.DefaultCellStyle.Font = new Font("Arial", 12, FontStyle.Regular);
+            dataGridView1.ColumnHeadersDefaultCellStyle.Font = new Font("Arial", 12, FontStyle.Bold);
+
+            
+            dataGridView1.DefaultCellStyle.ForeColor = Color.Black;
+            dataGridView1.DefaultCellStyle.BackColor = Color.White;
+        }
+
+        private void textBox_ID_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Submit_Click(object sender, EventArgs e)
+        {
+            Employee newEmployee = new Employee
+            {
+                ID = textBox_ID.Text,
+                FirstName = textBox_FIRSTNAME.Text,
+                LastName = textBox_LASTNAME.Text,
+                Position = textBox_Position.Text,
+            };
+
+            employees.Add(newEmployee);  
+            ClearInputFields();
+        }
+
+        private void ClearInputFields()
+        {
+            textBox_ID.Clear();
+            textBox_FIRSTNAME.Clear();
+            textBox_LASTNAME.Clear();
+            textBox_Position.Clear();
+        }
+    }
+
+    public class Employee
+    {
+
+        private string id;
+        private string firstName;
+        private string lastName;
+        private string position;
+
+        public string ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public string Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        
+        public Employee(string id, string firstName, string lastName, string position)
+        {
+            this.id = id;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.position = position;
+        }
+
+
+        public Employee() { }
+    }
+}
